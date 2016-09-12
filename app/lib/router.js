5dc8b28bccd41c6167ec19de1d9dac02c78a9abf
Evgeniy Tatarin <tatarin.e.m@gmail.com>
2016-09-12 17:28:36 +0300

    after review changes

--- app/lib/router.js
+++ app/lib/router.js
@@ -1,6 +1,4 @@
-require('../src/models/image.js');
-const newError = require('../lib/errorFactory');
-const routes = require('../routes.js');
+const routes = require('../routes.json');
 const express = require('express');
 const co = require('co');
 const images = require('../controllers/images');
@@ -10,12 +8,12 @@ const controllers = { images, git };
 const router = express.Router();
 
 Object.keys(routes).forEach((item) => {
-  let [httpMethod, path] = item.split(/ +/);
-  httpMethod = httpMethod.toLowerCase();
-  router[httpMethod](path, co.wrap(function*(req, res, next) {
-    const [controller, fn] = routes[item].split('.');
+  let [method, path] = item.split(/ +/);
+  method = method.toLowerCase();
+  router[method](path, co.wrap(function*(req, res, next) {
+    const [controller, action] = routes[item].split('.');
     try {
-      const result = yield co(controllers[controller][fn](req))
+      const result = yield co(controllers[controller][action](req))
       res.json(result)
     } catch (err) {
       return next(err);
